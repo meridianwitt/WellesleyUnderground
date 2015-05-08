0a804a0273ce72216ce56e30c6edfff666bc5c17
Meridian <mwitt@wellesley.edu>
2015-05-07 23:12:26 -0400

    Broke functionality
    Notes about inability to display results in console, inability to access
    session variable gotPosts, tumblrBlocks will not generate divs. Added d3
    js package. Added some of the hover functionality (not tested)

--- server/WU-server.js
+++ server/WU-server.js
@@ -6,19 +6,39 @@
 var oAuth = "wLtyjO5g0zQWJHFRycQxzWIMUjr3j1l16JpWr4aKirMFg6u8cL";
 var secret = "5UZ7LPnwK3QhWltoPbwSHc5g8VMSBFZEoc4dnv2ADlLm1g5IHA";
 
-var bioURL = "http://api.tumblr.com/v2/blog/wellesleyunderground.tumblr.com/info?api_key=wLtyjO5g0zQWJHFRycQxzWIMUjr3j1l16JpWr4aKirMFg6u8cL";
-var taggedURL1 = "http://api.tumblr.com/v2/blog/wellesleyunderground.tumblr.com/posts/?api_key=fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4"
+//var bioURL = "http://api.tumblr.com/v2/blog/wellesleyunderground.tumblr.com/info?api_key=wLtyjO5g0zQWJHFRycQxzWIMUjr3j1l16JpWr4aKirMFg6u8cL";
+var fullURL = "http://api.tumblr.com/v2/blog/wellesleyunderground.tumblr.com/posts/?&api_key=wLtyjO5g0zQWJHFRycQxzWIMUjr3j1l16JpWr4aKirMFg6u8cL"
+var URL = "http://api.tumblr.com/v2/blog/wellesleyunderground.tumblr.com/posts/?&limit=20&offset="
+var key = "&?api_key=wLtyjO5g0zQWJHFRycQxzWIMUjr3j1l16JpWr4aKirMFg6u8cL"
 
 var taggedURL2 = "wellesley%20in%20comedy"; //encode URI
 
   Meteor.methods({
     callTumblr:function(){
-        console.log("callTumblr")
-        var response = HTTP.call("GET", taggedURL1);
-        console.log("got response:", response);
+//        console.log("callTumblr")
+
+//    var retrieve_more = function (offset) {
+//        console.log("Starting retrieve_more function");
+//        console.log(URL + offset + key);
+//        var response = HTTP.call(URL + offset + key, function(error,result) { //supposed to be async
+//            if (result.data.response.posts.length == 20) {
+//                console.log("num of post: expected 20, got:" + result.data.response.posts.length);
+//                retrieve_more(offset + 20);
+//            } else{
+//                 console.log(response);
+//            }    return response; //return shuts it all down, do +=?
+//        });
+//    };
+//
+//    retrieve_more(0);
+//}        
+    
+        var response = HTTP.call("GET", fullURL);
+//        console.log("got response:", response);
+//        return response.data.response.posts; //now the objects are not properly parsed in the console.
         return response.data.response.posts;
-  
-    }
+
       
-  });
+//  });
+    }})
       
