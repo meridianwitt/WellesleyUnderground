5cf34ceb3f54e7c8e406ebd5920c3749926b8baf
Meridian <mwitt@wellesley.edu>
2015-05-10 17:54:56 -0400

    Trying to make divs clientside, not in HTML

--- client/WU-client.js
+++ client/WU-client.js
@@ -46,6 +46,26 @@ Template.posts.helpers({
     }
 })
 
+Template.allPosts.helpers({
+    makeDivs: function(){
+    
+    var gotPosts = Session.get("gotPosts");
+        console.log("Got seesion variable:" + gotPosts);
+       for(var i = 0; i<gotPosts.length; i++){ //erroring here. done before calling any of the other callTumblr functions, so i added if statement
+                var onePost = gotPosts[i];
+//                console.log(onePost.title);
+                var onePostDiv = document.createElement("div");
+                onePostDiv.setAttribute("class","well");
+                onePostDiv.innerHTML = "<span class='title'>"+onePost.title+"</span>" + "<p>" +onePost.body+"</p>";
+           
+	            $(this).hover(function() {$(this).addClass("hovering");}, function() { $(this).removeClass("hovering");}); //hover on this div
+           
+                var allPosts = document.getElementById("allPosts");
+                allPosts.appendChild(onePostDiv);
+            }
+    }
+})
+
 function findTags(arrayTag, curFilter){
     var index = arrayTag.indexOf(curFilter)
     if(index != -1){
