8cab3c3caea8bdc0f65ac33da5886a1173f17034
Meridian <mwitt@wellesley.edu>
2015-05-10 10:08:20 -0400

    In the middle of pulling the right num of posts
    Adding global variables

--- client/WU-client.js
+++ client/WU-client.js
@@ -1,19 +1,19 @@
- Posts = new Meteor.Collection('posts');
-
-  Session.setDefault("results", []);
-
-
-  Template.body.helpers({
-	total: function(){
-      return Session.get("totalPosts");
-    },
-    });
-
-  Template.body.events({
-     'click #button': function(){
- 		Meteor.call('callTumblr', function(error, result){
-        Session.get("results");
-        console.log(results)
- 		}
-	  )}
- 	})
+// Posts = new Meteor.Collection('posts');
+//
+//  Session.setDefault("results", []);
+//
+//
+//  Template.body.helpers({
+//	total: function(){
+//      return Session.get("totalPosts");
+//    },
+//    });
+//
+//  Template.body.events({
+//     'click #button': function(){
+// 		Meteor.call('callTumblr', function(error, result){
+//        Session.get("results");
+//        console.log(results)
+// 		}
+//	  )}
+// 	})
--- server/WU-server.js
+++ server/WU-server.js
@@ -6,7 +6,6 @@
 //var oAuth = "wLtyjO5g0zQWJHFRycQxzWIMUjr3j1l16JpWr4aKirMFg6u8cL";
 //var secret = "5UZ7LPnwK3QhWltoPbwSHc5g8VMSBFZEoc4dnv2ADlLm1g5IHA";
 //
-//var bioURL = "http://api.tumblr.com/v2/blog/wellesleyunderground.tumblr.com/info?api_key=wLtyjO5g0zQWJHFRycQxzWIMUjr3j1l16JpWr4aKirMFg6u8cL";
 //var taggedURL1 = "http://api.tumblr.com/v2/blog/wellesleyunderground.tumblr.com/posts/?&limit=20&offset=0&api_key=wLtyjO5g0zQWJHFRycQxzWIMUjr3j1l16JpWr4aKirMFg6u8cL"
 //
 //var taggedURL2 = "wellesley%20in%20comedy"; //encode URI
@@ -50,17 +49,38 @@
 Posts = new Mongo.Collection("posts");
 
 var URL = "http://api.tumblr.com/v2/blog/wellesleyunderground.tumblr.com/posts/?api_key=wLtyjO5g0zQWJHFRycQxzWIMUjr3j1l16JpWr4aKirMFg6u8cL&offset="  // notice that I made some changes to the URL
+var bioURL = "http://api.tumblr.com/v2/blog/wellesleyunderground.tumblr.com/info?api_key=wLtyjO5g0zQWJHFRycQxzWIMUjr3j1l16JpWr4aKirMFg6u8cL";
+var inDB; //number of posts in db
+var numPosts; //number of posts grabbed from Tumblr
+var difPosts; //difference between number of posts and number in the database
 
-function retrieve(offset){
+function retrieve(offset){ 
     var response = HTTP.call("GET", URL + offset)
-    console.log("offset=", offset, response.data.response.posts.length)
+//    console.log("offset=", offset, response.data.response.posts.length)
     for (var i in response.data.response.posts){
       Posts.insert( response.data.response.posts[i]) // store posts in the Posts collection
     }
 }
 
-  Meteor.startup(function(){  d
-    for (var i=0; i < 3; i++){
+//compare that number to the total number of posts already stored in the database (Posts.find().count())
+//if they are different, calculate that difference and use that to either specify number of times you'll need to repeat the request with a different offset, or if the number is less than 20, than use that number to specify the limit value. 
+
+
+
+  Meteor.startup(function(){  
+    
+    var getNumPosts = HTTP.call("GET", bioURL);
+    inDB = Posts.find().count();
+    numPosts = getNumPosts.data.response.blog.posts;
+    difPosts = numPosts - inDB;
+   /* Scenarios: difPosts = 0 (all posts are in) and don't call anymore, inDB less than numPosts and need to iterate more, specifically if inDB is less than numPosts and dif is less than 20, change the limit appropriately 
+    if()
+   */
+    console.log("The number of posts gathered: " + numPosts);
+    for (var i=0; i < numPosts/20; i++){
       retrieve(i*20)
-    }  
+    } 
+    
+//    console.log("Number of posts: " + Posts.find().count());
+    
   });
