7997187f8afa40b8ca37d6fb0688efe917b645ea
Meridian <mwitt@wellesley.edu>
2015-05-10 11:06:30 -0400

    Pulls "basically" the right number of Tumblr posts

--- server/WU-server.js
+++ server/WU-server.js
@@ -48,7 +48,7 @@
 
 Posts = new Mongo.Collection("posts");
 
-var URL = "http://api.tumblr.com/v2/blog/wellesleyunderground.tumblr.com/posts/?api_key=wLtyjO5g0zQWJHFRycQxzWIMUjr3j1l16JpWr4aKirMFg6u8cL&offset="  // notice that I made some changes to the URL
+var URL = "http://api.tumblr.com/v2/blog/wellesleyunderground.tumblr.com/posts/?api_key=wLtyjO5g0zQWJHFRycQxzWIMUjr3j1l16JpWr4aKirMFg6u8cL&offset=";  // notice that I made some changes to the URL
 var bioURL = "http://api.tumblr.com/v2/blog/wellesleyunderground.tumblr.com/info?api_key=wLtyjO5g0zQWJHFRycQxzWIMUjr3j1l16JpWr4aKirMFg6u8cL";
 var inDB; //number of posts in db
 var numPosts; //number of posts grabbed from Tumblr
@@ -56,7 +56,7 @@ var difPosts; //difference between number of posts and number in the database
 var offset = 0; //offset needs to change 20 everytime we call for posts so if we called 75 times, offset 75(20), not just 75
 
 function retrieve(offset){ 
-    var response = HTTP.call("GET", URL + offset)
+    var response = HTTP.call("GET", URL + offset);
 //    console.log("offset=", offset, response.data.response.posts.length)
     for (var i in response.data.response.posts){
       Posts.insert( response.data.response.posts[i]) // store posts in the Posts collection
@@ -66,25 +66,27 @@ function retrieve(offset){
 //compare that number to the total number of posts already stored in the database (Posts.find().count())
 //if they are different, calculate that difference and use that to either specify number of times you'll need to repeat the request with a different offset, or if the number is less than 20, than use that number to specify the limit value. 
 
-
-
-  Meteor.startup(function(){  
+  Meteor.startup(function(){  //worl on edge casees later, took too long on this
     
     var getNumPosts = HTTP.call("GET", bioURL);
     inDB = Posts.find().count();
+    console.log("Num in db: " + inDB);
     numPosts = getNumPosts.data.response.blog.posts;
+    console.log("Num posts: " + numPosts);
     difPosts = numPosts - inDB;
+    console.log("Dif posts: " + difPosts);
 
-   if(difPosts = null){ //or null?
-      for (var i=0; i < numPosts/20; i++){ //Eni had it as 3 to grab 60 posts
-      retrieve(i*20)
-    }
-//       else if (difPosts = 0){
-//        //
-//    } else {
-//    
-//    }
-        
+   if(difPosts == numPosts){ //or null?
+      console.log("Num iterations: " + numPosts/20);
+    for (var i=0; i < numPosts/20; i++){ //Eni had it as 3 to grab 60 posts
+      retrieve(i*20);
+    } 
+   }
+      console.log("Number in database after: " + inDB);
+  })
+
+  
+    
 //      
 //    console.log("The number of posts gathered: " + numPosts);
 //    for (var i=0; i < numPosts/20; i++){ //Eni had it as 3 to grab 60 posts
@@ -92,5 +94,4 @@ function retrieve(offset){
 //    } 
 //    
 //    console.log("Number of posts: " + Posts.find().count());
-    
-  });
+//  }*/
