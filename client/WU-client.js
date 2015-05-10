0b73554449fb68243b4ca7694ce6f89dbb2c3259
Meridian <mwitt@wellesley.edu>
2015-05-10 11:30:30 -0400

    Unfinished publish and subscribe functionality

--- client/WU-client.js
+++ client/WU-client.js
@@ -1,8 +1,3 @@
-// Posts = new Meteor.Collection('posts');
-//
-//  Session.setDefault("results", []);
-//
-//
 //  Template.body.helpers({
 //	total: function(){
 //      return Session.get("totalPosts");
@@ -17,3 +12,24 @@
 // 		}
 //	  )}
 // 	})
+
+//when server is ready
+ Meteor.subscribe('thePosts');
+
+Meteor.startUp({
+   
+    var gotPosts = Session.get("gotPosts");
+            console.log("Got session variable:" + gotPosts);
+           for(var i = 0; i<gotPosts.length; i++){ //erroring here. done before calling any of the other callTumblr functions, so i added if statement
+                    var onePost = gotPosts[i];
+    //                console.log(onePost.title);
+                    var onePostDiv = document.createElement("div");
+                    onePostDiv.setAttribute("class","well");
+                    onePostDiv.innerHTML = "<span class='title'>"+onePost.title+"</span>" + "<p>" +onePost.body+"</p>";
+
+                    $(this).hover(function() {$(this).addClass("hovering");}, function() { $(this).removeClass("hovering");}); //hover on this div
+
+                    var allPosts = document.getElementById("allPosts");
+                    allPosts.appendChild(onePostDiv);
+                }
+})
