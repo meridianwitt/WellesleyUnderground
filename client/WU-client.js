e32fafc190eed29fb969ed76643e07ee6d0df354
Meridian <mwitt@wellesley.edu>
2015-05-10 12:29:01 -0400

    Beginning the div creation functionality

--- client/WU-client.js
+++ client/WU-client.js
@@ -14,22 +14,27 @@
 // 	})
 
 //when server is ready
- Meteor.subscribe('thePosts');
+Meteor.subscribe('thePosts');
+Posts = new Mongo.Collection("posts");
 
-Meteor.startUp({
-   
-    var gotPosts = Session.get("gotPosts");
-            console.log("Got session variable:" + gotPosts);
-           for(var i = 0; i<gotPosts.length; i++){ //erroring here. done before calling any of the other callTumblr functions, so i added if statement
-                    var onePost = gotPosts[i];
-    //                console.log(onePost.title);
-                    var onePostDiv = document.createElement("div");
-                    onePostDiv.setAttribute("class","well");
-                    onePostDiv.innerHTML = "<span class='title'>"+onePost.title+"</span>" + "<p>" +onePost.body+"</p>";
+Template.hello.helpers({
+   postList: function(){
+    return Posts.find({});
+   },
+    
+   makeDivs: function(){
+   for(var i = 0; i<Posts.length; i++){ //erroring here. done before calling any of the other callTumblr functions, so i added if statement
+            var onePost = Posts[i];
+//                console.log(onePost.title);
+            var onePostDiv = document.createElement("div");
+            onePostDiv.setAttribute("class","well");
+            onePostDiv.innerHTML = "<span class='title'>"+onePost.title+"</span>" + "<p>" +onePost.body+"</p>";
 
-                    $(this).hover(function() {$(this).addClass("hovering");}, function() { $(this).removeClass("hovering");}); //hover on this div
+            $(this).hover(function() {$(this).addClass("hovering");}, function() { $(this).removeClass("hovering");}); //hover on this div
 
-                    var allPosts = document.getElementById("allPosts");
-                    allPosts.appendChild(onePostDiv);
-                }
+            var allPosts = document.getElementById("allPosts");
+            allPosts.appendChild(onePostDiv);
+        }
+   }
 })
+    
