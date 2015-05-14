7109395ee6c9554fbb16d72ab85bbca3396377f5
Meridian <mwitt@wellesley.edu>
2015-05-14 13:46:09 -0400

    Now just using sess to dynamically count list

--- client/WU-client.js
+++ client/WU-client.js
@@ -30,6 +30,7 @@ Template.nav.events({ //give all the buttons a filter class, innerHTML will be u
 Template.posts.helpers({ 
    postList: function(){ //received session variable and filters accordingly
        var curFilter = Session.get("filter");
+       
     
        var counter = 0;
         if (curFilter != "") {
@@ -43,15 +44,30 @@ Template.posts.helpers({
 //                    Posts.update(this, {filter: true})}})
 //                    return Posts.find({filter: true});
 //                Meteor.call('filtered', returnSess());
+                Session.set("numPosts", Posts.find({tags: {$in: [curFilter]}}).count())
                 return Posts.find({tags: {$in: [curFilter]}}); 
 //              return PostListF; //need a method to find button value in the array of tags
         } else {
+        Session.set("numPosts", Posts.find().count())
         return Posts.find(); //Posts.find() works here
 //        counter = Posts.find().count
         }
+   },
+            
+    tagList: function(){
+        return Tags.find({}, {sort: {count: -1}});
+    },
+    
+    postListCount: function(){
+        return Session.get("numPosts");
+    },
+    
+    tagCount: function(button){
+        var tag = Tags.find({name: "wellesley"}).fetch();
+        return tag[0].count;
+    },
     
 //    return Posts.find({});
-   },
     body1: function(){ ///FIX FOR POSTS WITH IMAGES, ones with pictures begin with undefined?
         //an image
         //var wholeBody = actualBody;
@@ -82,14 +98,28 @@ Template.posts.helpers({
 
 Template.nav.helpers({ //don't know how to load this at the appropriate time
     counter: function(buttonVal){ //another way to use one counter helper method?
-//        Meteor.startup({
-//        return Posts.find({tags: {$in: [buttonVal]}}).count()
-        Session.set("tags",Tags.find({name: buttonVal}, {count: true, _id:0})) //find the count of a particular tags
-//        Session.set("tags",Tags.find({name: buttonVal})) //find the count of a particular tags
-//        console.log("Tags: " + Tags.find({name: buttonVal}, {count: true, _id:0}))  
-        return Session.get("tags");
+//        var tag = Tags.find({name: buttonVal}) //undefined?
+//        
+//        console.log("Why is this tag undefined: " + tag)
+//        console.log("Why is this inside the array: " + tag[0])
+//        console.log("Why is this count undefined: " + tag[0].count)
+//        return tag[0].count //find the count of a particular tags
+
+//        return Tags.find({name: buttonVal}, {count: true, _id:0}) //find the count of a particular tags
+//        Session.set("tags",Tags.find({name: buttonVal}, {count: true, _id:0})) //find the count of a particular tags
+
+//        return Session.get("tags");
 //    })
     }
+})
+
+Template.nav.events({
+    'keypress #ex3': function(){
+//       
+            var value = $("#ex3").val(this.value)
+            $( "#typed" ).text( value );
+          }
+
 })
 
 //Template.d3.helpers({
@@ -99,27 +129,6 @@ Template.nav.helpers({ //don't know how to load this at the appropriate time
 //    .append
 //})
 
-//Template.nav.OnRendered(function(buttonVal) { //deprecated
-//    return Posts.find({tags: {$in: [buttonVal]}}).count()
-////    })
-//}
-//
-//function findTags(onePost, curFilter){ //findTags(Posts[i], ignoreCase
-//    arrayTags = onePost.tags;
-//    var index;
-//    for(var i in arrayTags){ //try to ignore case 
-//      arrayTags[i] = arrayTags[i].toLowerCase();}
-//      console.log("array tags: " + arrayTags);
-//      index = $.inArray(curFilter, arrayTags);
-//        if(index != -1){
-//            console.log("got true");
-//            return true;
-//        } else {
-//            console.log("got false");
-//            return false;
-//        }  
-//    }
-    
 function returnSess(){
     return Session.get("filter");
 }
